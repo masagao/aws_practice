39242194e2c17a7505e1b7f8ab58180966a44ee2
masagao <masanao@gaogao.asia>
2021-04-29 21:32:55 +0900

    add tests

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -2,21 +2,35 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ArticleControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /**
+     * @test
+     */
+    public function testIndex() {
+        $response = $this->get(route('articles.index'));
+
+        $response->assertStatus(200)->assertViewIs('articles.index');
+    }
+
+    public function testGuestCreate() {
+        $response = $this->get(route('articles.create'));
+
+        $response->assertRedirect(route('login'));
+    }
+
+    public function testAuthCreate() {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('articles.create'));
+
+        $response->assertStatus(200)->assertViewIs('articles.create');
     }
 }
